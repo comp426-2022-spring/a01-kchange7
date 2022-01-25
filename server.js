c521a6a66c411f6be2de5552013942e331d36100
kchange7 <69439037+kchange7@users.noreply.github.com>
2022-01-25 15:52:33 -0500

    added fs.readFile section

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ const port = args.port || 3000
 // Use the documentation for the Node.js `fs` module.
 // The function must read a file located at `./www/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-
-// If there is an error, put it on the console error, return, and exit with error code 1.
-// Do not be nice about exiting.
-
-
-
-
-
+fs.readFile('./www/index.html', 'utf8', (err, data) => {
+    // If there is an error, put it on the console error, return, and exit with error code 1.
+    // Do not be nice about exiting.
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+})
 // Define a const `server` as an arrow function using http.createServer.
 // Use the documentation for the node.js http module.
 // The function should have three responses:
