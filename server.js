e92dc2aec92b61894b8e7c942d00755de6fb8ee3
kchange7 <69439037+kchange7@users.noreply.github.com>
2022-01-27 15:07:20 -0500

    added return on error

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ fs.readFile('./www/index.html', 'utf8', (err, data) => {
     // Do not be nice about exiting.
     if (err) {
         console.error(err)
+        return
         process.exit(1)
     }
     // Define a const `server` as an arrow function using http.createServer.
